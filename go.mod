873c7dd9352805373a1dfaf8e1613bfc44706d58
yujie <825407762@qq.com>
2021-05-30 22:01:31 +0800

    -d

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.4
 	github.com/sirupsen/logrus v1.8.1
+	github.com/takama/daemon v1.0.0
 	gopkg.in/gcfg.v1 v1.2.3
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- learn/zhiz.go
+++ learn/zhiz.go
@@ -1,39 +1,138 @@
+// Example of a daemon with echo service
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
-type Books struct {
-	title   string
-	author  string
-	subject string
-	book_id int
+	"github.com/takama/daemon"
+)
+
+const (
+
+	// name of the service
+	name        = "myservice"
+	description = "My Echo Service"
+
+	// port which daemon should be listen
+	port = ":9977"
+)
+
+//    dependencies that are NOT required by the service, but might be used
+var dependencies = []string{"dummy.service"}
+
+var stdlog, errlog *log.Logger
+
+// Service has embedded daemon
+type Service struct {
+	daemon.Daemon
 }
 
-func (e *Books) name() {
-	e.title = "qweasd"
+// Manage by daemon commands or run the daemon
+func (service *Service) Manage() (string, error) {
+
+	usage := "Usage: myservice install | remove | start | stop | status"
+
+	// if received any kind of command, do it
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "install":
+			return service.Install()
+		case "remove":
+			return service.Remove()
+		case "start":
+			return service.Start()
+		case "stop":
+			return service.Stop()
+		case "status":
+			return service.Status()
+		default:
+			return usage, nil
+		}
+	}
+
+	// Do something, call your goroutines, etc
+
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	// Set up listener for defined host and port
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		return "Possibly was a problem with the port binding", err
+	}
+
+	// set up channel on which to send accepted connections
+	listen := make(chan net.Conn, 100)
+	go acceptConnection(listener, listen)
+
+	// loop work cycle with accept connections or interrupt
+	// by system signal
+	for {
+		select {
+		case conn := <-listen:
+			go handleClient(conn)
+		case killSignal := <-interrupt:
+			stdlog.Println("Got signal:", killSignal)
+			stdlog.Println("Stoping listening on ", listener.Addr())
+			listener.Close()
+			if killSignal == os.Interrupt {
+				return "Daemon was interruped by system signal", nil
+			}
+			return "Daemon was killed", nil
+		}
+	}
+
+	// never happen, but need to complete code
+	return usage, nil
 }
 
-func main2() {
-	var b1 Books /* 声明 b1 为 Books 类型 */
-	var b2 Books /* 声明 b2 为 Books 类型 */
+// Accept a client connection and collect it in a channel
+func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		listen <- conn
+	}
+}
 
-	/* book 1 描述 */
-	b1.title = "Go 语言"
-	b1.author = "www.runoob.com"
-	b1.subject = "Go 语言教程"
-	b1.book_id = 6495407
+func handleClient(client net.Conn) {
+	for {
+		buf := make([]byte, 4096)
+		numbytes, err := client.Read(buf)
+		if numbytes == 0 || err != nil {
+			return
+		}
+		client.Write(buf[:numbytes])
+	}
+}
 
-	/* book 2 描述 */
-	b2.title = "Python 教程"
-	b2.author = "www.runoob.com"
-	b2.subject = "Python 语言教程"
-	b2.book_id = 6495700
+func init() {
+	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+}
 
-	fmt.Println(b1)
-	fmt.Println(b2)
-
-	b1.name()
-
-	fmt.Println(b1)
-	fmt.Println(b2)
+func main() {
+	srv, err := daemon.New(name, description, daemon.SystemDaemon, dependencies...)
+	if err != nil {
+		errlog.Println("Error: ", err)
+		os.Exit(1)
+	}
+	service := &Service{srv}
+	status, err := service.Manage()
+	if err != nil {
+		errlog.Println(status, "\nError: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
 }
