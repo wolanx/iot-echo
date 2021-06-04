2e7cf3df80e0baa5f1f58685a7480ea3fb971ca0
yujie <825407762@qq.com>
2021-06-04 21:02:57 +0800

    daemon

--- learn/zhiz.go
+++ learn/zhiz.go
@@ -1,10 +1,9 @@
-// Example of a daemon with echo service
 package main
 
 import (
 	"fmt"
 	"log"
-	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,29 +12,53 @@ import (
 )
 
 const (
-
-	// name of the service
-	name        = "myservice"
+	name        = "iot-echo"
 	description = "My Echo Service"
-
-	// port which daemon should be listen
-	port = ":9977"
 )
 
-//    dependencies that are NOT required by the service, but might be used
-var dependencies = []string{"dummy.service"}
+var dependencies []string
 
-var stdlog, errlog *log.Logger
+var stdout, stderr *log.Logger
 
-// Service has embedded daemon
+func init() {
+	stdout = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	stderr = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+}
+
+// cd Z:\www\src\github.com\zx5435\iot-echo\learn
+func main() {
+	srv, err := daemon.New(name, description, daemon.SystemDaemon, dependencies...)
+	if err != nil {
+		stderr.Println("Error: ", err)
+		os.Exit(1)
+	}
+	service := &Service{srv}
+	status, err := service.Manage()
+	if err != nil {
+		stderr.Println(status, "\nError: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
+}
+
+// Accept a client connection and collect it in a channel
+func acceptConnection() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Hello World!"))
+}
+
 type Service struct {
 	daemon.Daemon
 }
 
 // Manage by daemon commands or run the daemon
 func (service *Service) Manage() (string, error) {
-
-	usage := "Usage: myservice install | remove | start | stop | status"
+	usage := "Usage: install | remove | start | stop | status"
 
 	// if received any kind of command, do it
 	if len(os.Args) > 1 {
@@ -52,87 +75,30 @@ func (service *Service) Manage() (string, error) {
 		case "status":
 			return service.Status()
 		default:
+			status, err := service.Status()
+			if err != nil {
+				return "", err
+			}
+			stdout.Println(status)
+
+			stdout.Println("	this body")
+
+			sig := make(chan os.Signal, 1)
+			done := make(chan bool, 1)
+			signal.Notify(sig, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+			go func() {
+				go acceptConnection()
+				<-sig
+				done <- true
+			}()
+
+			stdout.Println("awaiting sig")
+			<-done
+			stdout.Println("exiting")
 			return usage, nil
 		}
 	}
 
-	// Do something, call your goroutines, etc
-
-	// Set up channel on which to send signal notifications.
-	// We must use a buffered channel or risk missing the signal
-	// if we're not ready to receive when the signal is sent.
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
-
-	// Set up listener for defined host and port
-	listener, err := net.Listen("tcp", port)
-	if err != nil {
-		return "Possibly was a problem with the port binding", err
-	}
-
-	// set up channel on which to send accepted connections
-	listen := make(chan net.Conn, 100)
-	go acceptConnection(listener, listen)
-
-	// loop work cycle with accept connections or interrupt
-	// by system signal
-	for {
-		select {
-		case conn := <-listen:
-			go handleClient(conn)
-		case killSignal := <-interrupt:
-			stdlog.Println("Got signal:", killSignal)
-			stdlog.Println("Stoping listening on ", listener.Addr())
-			listener.Close()
-			if killSignal == os.Interrupt {
-				return "Daemon was interruped by system signal", nil
-			}
-			return "Daemon was killed", nil
-		}
-	}
-
-	// never happen, but need to complete code
-	return usage, nil
-}
-
-// Accept a client connection and collect it in a channel
-func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		listen <- conn
-	}
-}
-
-func handleClient(client net.Conn) {
-	for {
-		buf := make([]byte, 4096)
-		numbytes, err := client.Read(buf)
-		if numbytes == 0 || err != nil {
-			return
-		}
-		client.Write(buf[:numbytes])
-	}
-}
-
-func init() {
-	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime)
-}
-
-func main() {
-	srv, err := daemon.New(name, description, daemon.SystemDaemon, dependencies...)
-	if err != nil {
-		errlog.Println("Error: ", err)
-		os.Exit(1)
-	}
-	service := &Service{srv}
-	status, err := service.Manage()
-	if err != nil {
-		errlog.Println(status, "\nError: ", err)
-		os.Exit(1)
-	}
-	fmt.Println(status)
+	return "nono", nil
 }
